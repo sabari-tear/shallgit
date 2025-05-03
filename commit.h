b97f269957780c010cb7885d2d8568d9db2bc0d4
Sabari3000 <sabarinathan30012005@gmail.com>
2025-05-03 15:37:51 +0530

    initial commit

--- commit.h
+++ commit.h
@@ -1,9 +1,17 @@
 #pragma once
 
 #include <unordered_map>
+#include <string>
+#include <chrono>
 using namespace std;
 class commit
 {
+	string message;
+	unordered_map<string, string> blobs;
+	string parentHash;
+	string datetime;
+	string ownHash;
+
 	commit();
 	commit(string& msg, unordered_map<string, string>& blobMap, string& parent);
 };
