6e0a1f4778948a2ca940dd314ba593a3671bd3b1
Sabari3000 <sabarinathan30012005@gmail.com>
2025-05-04 16:08:26 +0530

    commit access decided

--- commit.h
+++ commit.h
@@ -15,6 +15,11 @@ public:
 	commit(const std::string& msg, const std::unordered_map<std::string, std::string>& blobMap, const std::string& parent);
 
 	std::string getOwnHash();
-
+	std::string calcHash();
+	std::string getParentHash();
+	std::string getMessage();
+	std::string getDatetime();
+	std::unordered_map<std::string, std::string> getBlobs();
+	string globalLog();
 };
 
