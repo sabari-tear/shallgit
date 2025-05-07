1bdaf9dc18da69c7f459b197ae82b6a87d07ee3f
Sabari3000 <sabarinathan30012005@gmail.com>
2025-05-07 16:44:12 +0530

    commit.h done

--- commit.cpp
+++ commit.cpp
@@ -5,8 +5,8 @@ commit::commit() {
 
 commit::commit(const std::string& msg, const std::unordered_map<std::string, std::string>& blobMap, const std::string& parent)
 	:message(msg), blobs(blobMap), parentHash(parent) {
-	datetime = "";//currentDateTime();
-	ownHash = ""; //calcHash();
+	datetime = currentDateTime();
+	ownHash = calcHash();
 }
 
 std::string commit::getOwnHash() {
