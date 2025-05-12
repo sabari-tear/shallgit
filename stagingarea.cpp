07e9e298e55e858fefa2053469056398639e2932
Sabari3000 <sabarinathan30012005@gmail.com>
2025-05-12 10:56:44 +0530

    included add and remove to staging

--- stagingarea.cpp
+++ stagingarea.cpp
@@ -3,3 +3,6 @@
 void stagingarea::add(const std::string& filename, const std::string& sha1) {
 	addedFiles[filename] = sha1;
 }
+void stagingarea::addToRemovedFiles(const std::string& filename) {
+	removedFiles.push_back(filename);
+}
