450d847b8c3268c181aea160cc342fc2917ad69b
Sabari3000 <sabarinathan30012005@gmail.com>
2025-05-12 10:58:59 +0530

    added staging access methods

--- stagingarea.h
+++ stagingarea.h
@@ -6,10 +6,8 @@ class stagingarea
 public:
 	stagingarea();
 	
-	/*
 	void add(const std::string& filename, const std::string& sha1);
 	void addToRemovedFiles(const std::string& filename);
-	*/
 
 	std::unordered_map<std::string, std::string> addedFiles;
 	std::vector<std::string> removedFiles;
