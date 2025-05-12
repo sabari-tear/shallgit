75a31d1194504abae4f3b586e8df46f13025fdfa
Sabari3000 <sabarinathan30012005@gmail.com>
2025-05-12 10:57:57 +0530

    removed extra methods

--- stagingarea.cpp
+++ stagingarea.cpp
@@ -1,8 +1,2 @@
 #include "stagingarea.h"
 
-void stagingarea::add(const std::string& filename, const std::string& sha1) {
-	addedFiles[filename] = sha1;
-}
-void stagingarea::addToRemovedFiles(const std::string& filename) {
-	removedFiles.push_back(filename);
-}
